bff54c81466a9c46f8cefbff1ac3b9a3cdf18dff
Aliha07 <alihaqaiser50@gmail.com>
2024-11-28 13:24:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ahmad
+ its html js base 
---
+++ index.js
@@ -0,0 +1,7 @@
+alert('hi ahmad') ;
+//string "abkgu.k,njbgk.uifgvjhk"
+alert(225) ;
+alert(false) ;
+a = prompt('enter your name')
+alert(a)
+document.write(a)
